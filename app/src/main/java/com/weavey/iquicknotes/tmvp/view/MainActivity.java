8918a786e26232818b30518646251b6a7caad8c5
Weavey <vv.136@qq.com>
2016-11-30 00:03:28 +0800

    rename  mainactivity

--- app/src/main/java/com/weavey/iquicknotes/tmvp/view/MainActivity.java
+++ app/src/main/java/com/weavey/iquicknotes/tmvp/view/MainActivity.java
@@ -1,7 +1,9 @@
-package com.weavey.iquicknotes;
+package com.weavey.iquicknotes.tmvp.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.weavey.iquicknotes.R;
 
 public class MainActivity extends AppCompatActivity {
 
